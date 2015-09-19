4a6e43d215c7d7d42867db377da3275be9a20b30
Chelsea Kwong <chelseakct@gmail.com>
2015-09-19 13:02:55 -0400

    fixed sizing and hover

---
+++ js/yummly.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    $( ".col-md-3" ).hover(
+  function() {
+    $(this).addClass( "hover" );
+  }, function() {
+    $(this).removeClass( "hover" );
+  }
+);
+});
+ 
